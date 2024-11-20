4dc40fa8cae70f844de5844ed538783e303c5845
fkori17 <fkori0217@gmail.com>
2024-11-20 17:38:06 +0100

    Script 2.0 verzió, Új design file
    - Külön hosszabbítás számláló hozzáadva
    - A design új verziójának PDF-je hozzáadva, a régi törölve
    - A félidők hosszának megadható limit módosítva

--- script2.js
+++ script2.js
@@ -2,6 +2,8 @@ document.getElementById("t1Title").textContent = localStorage.getItem("team1");
 document.getElementById("t2Title").textContent = localStorage.getItem("team2");
 let halftime = localStorage.getItem("halftime");
 let totalTime = halftime * 60;
+let matchTime = 0;
+let overtime = 0;
 
 // Point management
 function addPoint(id) {
@@ -22,39 +24,42 @@ let startButton = document.getElementById("startMatchButton");
 let stopButton = document.getElementById("stopMatchButton");
 let overtimeButton = document.getElementById("overtimeButton");
 let timeDisplay = document.getElementById("timeDisplay");
+let overtimeDisplay = document.getElementById("overtimeDisplay");
 
 function updateTimeDisplay() {
-    const minutes = Math.floor(totalTime / 60).toString().padStart(2, '0');
-    const seconds = (totalTime % 60).toString().padStart(2, '0');
+    const minutes = Math.floor(matchTime / 60).toString().padStart(2, '0');
+    const seconds = (matchTime % 60).toString().padStart(2, '0');
     timeDisplay.textContent = `${minutes}:${seconds}`;
 }
 updateTimeDisplay();
+
 
 let timerInterval;
 function startMatch() {
     startButton.disabled = true;
     stopButton.disabled = false;
     timerInterval = setInterval(() => {
-        if (totalTime > 0) {
-            totalTime--;
+        if (matchTime < totalTime) {
+            matchTime++;
             updateTimeDisplay();
-            if (totalTime < 300 && !isOvertime) {
-                overtimeButton.disabled = false;
-            } 
+        } else if (overtime > 0) {
+            overtime--;
+            updateOvertimeDisplay();
         } else {
             clearInterval(timerInterval);
             if (firstHalf) {
                 firstHalf = false;
-                totalTime = halftime;
+                matchTime = 0;
                 alert("Véget ért az első félidő!");
                 document.getElementById("halfTimeDisplay").textContent = "2. félidő"
                 startButton.disabled = false;
                 stopButton.disabled = true;
-                isOvertime = false;
+                overtimeButton.disabled = false;
                 updateTimeDisplay();
             } else {
                 alert("Véget ért a mérkőzés!")
                 stopButton.disabled = true;
+                overtimeButton.disabled = true;
             }
     
         }
@@ -67,18 +72,22 @@ function stopMatch() {
     clearInterval(timerInterval);
 }
 
+
+function updateOvertimeDisplay() {
+    const minutes = Math.floor(overtime / 60).toString().padStart(2, '0');
+    const seconds = (overtime % 60).toString().padStart(2, '0');
+    overtimeDisplay.textContent = `${minutes}:${seconds}`;
+}
 function addOvertime() {
     overtimeInput = parseInt(document.getElementById("overtimeInput").value);
-    if (overtimeInput > 0 && overtimeInput < halftime) {
-        isOvertime = true;
+    if (overtimeInput > 0 && overtimeInput <= halftime) {
         overtimeButton.disabled = true;
-        totalTime = totalTime + overtimeInput * 60;
-        overtimeInput = "";
-        updateTimeDisplay();
+        overtime = overtimeInput * 60;
+        updateOvertimeDisplay();
     }
-    
-
 }
+
+
 
 function cycleCard(teamnumber) {
     cardselect =document.getElementById(`t${teamnumber}CardSelect`);
